49af20a6324617ff42b6e4e27a91cd90159b2b90
punitdekate <punitdekate.1999@gmail.com>
2024-04-04 15:14:55 +0530

    deploy after chage in config

--- src/config/mongoose.config.js
+++ src/config/mongoose.config.js
@@ -4,11 +4,13 @@ import '../config/env.config.js'
 
 const url = process.env.DB_URL;
 
-const connectMongoose = () => {
-    mongoose.connect(url).then((clientInstance => {
-        console.log("MongoDb is connected using mongoose")
-    })).catch(error => {
+const connectMongoose = async() => {
+    try {
+        await mongoose.connect(url);
+        console.log("MongoDb is connected using mongoose");
+    } catch (error) {
+        console.log(error)
         throw new CustomError("There is some issue in database connection", 400)
-    })
+    }
 }
 export default connectMongoose;
