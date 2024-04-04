13f55d32a72abcfcf5d6a56846e5cf43955d5f84
punitdekate <punitdekate.1999@gmail.com>
2024-04-04 15:02:17 +0530

    changes in config

--- src/config/mongoose.config.js
+++ src/config/mongoose.config.js
@@ -4,8 +4,8 @@ import '../config/env.config.js'
 
 const url = process.env.DB_URL;
 
-const connectMongoose = async() => {
-    await mongoose.connect(url).then((clientInstance => {
+const connectMongoose = () => {
+    mongoose.connect(url).then((clientInstance => {
         console.log("MongoDb is connected using mongoose")
     })).catch(error => {
         throw new CustomError("There is some issue in database connection", 400)
